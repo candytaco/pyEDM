358dc0450ebae2c1bcb468be542b51aeaf06080c
tzhang <t.zhang@berkeley.edu>
2025-12-19 17:31:17 -0800

    update init file for new architecture with objects

--- src/pyEDM/__init__.py
+++ src/pyEDM/__init__.py
@@ -7,6 +7,35 @@ from .Examples import Examples
 from .Utils import SurrogateData
 from .Embed import Embed
 from .LoadData import sampleData
+# Import parameter configuration objects
+from .Parameters import (
+    EDMParameters,
+    DataSplit,
+    GenerationParameters,
+    SMapParameters,
+    CCMParameters,
+    MultiviewParameters,
+    ExecutionParameters
+)
+# Import result objects
+from .Results import (
+    SimplexResult,
+    SMapResult,
+    CCMResult,
+    MultiviewResult
+)
+# Import visualization functions
+from .Visualization import (
+    plot_prediction,
+    plot_smap_coefficients,
+    plot_ccm,
+    plot_multiview,
+    plot_embed_dimension,
+    plot_predict_interval,
+    plot_predict_nonlinear
+)
+# Import execution configuration
+from .Execution import ExecutionMode
 
 __version__     = "2.3.2"
 __versionDate__ = "2025-11-17"
